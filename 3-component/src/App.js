981bd15f6b5659306c6014264052effe6e4fd43a
hongui <honge7694@naver.com>
2024-06-09 20:50:31 +0900

    [구현하기 1] SearchForm 1

--- 3-component/src/App.js
+++ 3-component/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from './components/Header';
+import SearchForm from './components/SearchForm';
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<>
-				<Header title={"검색"}/>
+				<Header title="검색"/>
+				<div className='container'>
+					<SearchForm />
+				</div>
 			</>
 		);
 	}
