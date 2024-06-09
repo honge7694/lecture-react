91a6ba6d0566e92f5e84aec49785b2a891546856
hongui <honge7694@naver.com>
2024-06-09 21:21:51 +0900

    [구현하기 1] SearchForm 2

--- 3-component/src/App.js
+++ 3-component/src/App.js
@@ -3,12 +3,23 @@ import Header from './components/Header';
 import SearchForm from './components/SearchForm';
 
 export default class App extends React.Component {
+	search(searchKeyword) {
+		console.log("TODO: ", searchKeyword);
+	}
+	
+	handleReset() {
+		console.log("TODO: handleReset");
+	}
+	
 	render() {
 		return (
 			<>
 				<Header title="검색"/>
 				<div className='container'>
-					<SearchForm />
+					<SearchForm 
+						onSubmit={(searchKeyword) => this.search(searchKeyword)} 
+						onReset={() => this.handleReset()}
+					/>
 				</div>
 			</>
 		);
